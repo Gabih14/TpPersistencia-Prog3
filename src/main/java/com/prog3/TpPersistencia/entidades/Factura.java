e1cef26902c4494041c8315ead8282de062989fe
Gabriel Hernandez <hernandezg.alvarez@gmail.com>
2023-09-11 01:20:55 -0300

    cambia el enum.type a STRING

--- src/main/java/com/prog3/TpPersistencia/entidades/Factura.java
+++ src/main/java/com/prog3/TpPersistencia/entidades/Factura.java
@@ -2,6 +2,8 @@ package com.prog3.TpPersistencia.entidades;
 
 import com.prog3.TpPersistencia.enums.FormaPago;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +23,7 @@ public class Factura extends BaseEntidad{
     private LocalDate fecha;
     private int numero;
     private double descuento;
+    @Enumerated(EnumType.STRING)
     private FormaPago formaPago;
     private int total;
 }
